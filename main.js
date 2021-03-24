b0c6d9a30e48e5ad7765e52ee5b1adb2a154bb9b
zJuarez <mji2000.741@gmail.com>
2021-03-24 15:21:45 -0600

    changed to vue 3 introduction course LOL

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-var app = new Vue({
-      el: '#app',
-      data: {
-        product: 'Socks',
-        description: 'A pair of warm fuzzy socks',
-        image: './assets/socks.jpg',
-        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
-      } 
-    })
+const product = 'Socks'
