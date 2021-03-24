7ed08773570b319a3e42ee3ca9d97023c29f212c
zJuarez <mji2000.741@gmail.com>
2021-03-24 15:57:04 -0600

    video 5 finished

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ const app = Vue.createApp({
   data() {
     return {
       product: 'Socks',
-      description: 'I love it'
+      description: 'I love it',
+      image: './assets/images/socks_blue.jpg',
+      link: 'https://www.vuemastery.com/',
+      inventory : 80,
+      details: ['50% cotton', '30% WebGLUniformLocation', '20% polyester'],
+      variants: [
+        {id: 2234, color: 'green'},
+        {id: 2235, color: 'blue'},
+      ],
     }
   }
 })
