6a5ccd85cc2cffac6428ce0d21a1cc598e521d54
zJuarez <mji2000.741@gmail.com>
2021-03-24 16:07:03 -0600

    ended video 6

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: 'Socks',
       description: 'I love it',
-      image: './assets/images/socks_blue.jpg',
       link: 'https://www.vuemastery.com/',
       inventory : 80,
+      image: './assets/images/socks_green.jpg',
       details: ['50% cotton', '30% WebGLUniformLocation', '20% polyester'],
       variants: [
-        {id: 2234, color: 'green'},
-        {id: 2235, color: 'blue'},
+        {id: 2234, color: 'green', image: './assets/images/socks_green.jpg',},
+        {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg',},
       ],
+    }
+  },
+  methods: {
+    addToCart(){
+      this.cart += 1
+    },
+    updateImage(activeImage){
+      this.image = activeImage
     }
   }
 })
